8ca007fe587e4de2a3eebc0421921b70d486a3d3
xaiur <davinoverclock@gmail.com>
2022-12-04 12:24:02 +0800

    Fade Transition

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -7,31 +7,21 @@ import '../controllers/home_controller.dart';
 class HomeView extends GetView<HomeController> {
   double myKubus = Get.height / 3;
 
-  DecorationTween myDec = DecorationTween(
-    begin: BoxDecoration(
-      color: Colors.pink,
-      // rumus border radius bagus panjang-lebar or sisi / 2 / 3
-      borderRadius: BorderRadius.circular(Get.height / 3 * 0.5 / 3),
-    ),
-    end: BoxDecoration(
-      color: Colors.lightGreenAccent,
-      borderRadius: BorderRadius.circular(Get.height / 3 * 0.5),
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Animated Box Transition'),
+        title: Text('Fade Transition'),
         centerTitle: true,
       ),
       body: Center(
-        child: DecoratedBoxTransition(
-          decoration: myDec.animate(controller.animationC),
+        child: FadeTransition(
+          opacity: CurvedAnimation(
+              parent: controller.animationC, curve: Curves.fastOutSlowIn),
           child: Container(
             width: myKubus,
             height: myKubus,
+            color: Colors.red,
           ),
         ),
       ),
