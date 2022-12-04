8eee89d388d08a070897c55be617c1ef24f9f51b
xaiur <davinoverclock@gmail.com>
2022-12-04 14:21:27 +0800

    radius modif

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -13,17 +13,20 @@ class HomeView extends GetView<HomeController> {
         centerTitle: true,
       ),
       body: Center(
-        child: Container(
-          height: 300,
-          width: 300,
-          decoration: BoxDecoration(
-            color: Colors.grey.shade300,
-            borderRadius: BorderRadius.circular(
-              150,
-            ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(5),
+            topRight: Radius.circular(5),
+            bottomRight: Radius.circular(50),
+            bottomLeft: Radius.circular(5),
           ),
-          child: FlutterLogo(
-            size: 300,
+          child: Container(
+            height: 300,
+            width: 300,
+            color: Colors.grey.shade300,
+            child: FlutterLogo(
+              size: 300,
+            ),
           ),
         ),
       ),
