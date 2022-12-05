81c0b1ffa37fa29f576c0b89570bf28c1aca4e05
xaiur <davinoverclock@gmail.com>
2022-12-05 19:45:04 +0800

    Custom Pain

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -11,30 +11,51 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Backdrop Filter'),
+        title: Text('Custom Pain'),
         centerTitle: true,
       ),
       body: Center(
-        child: Stack(
-          children: [
-            Container(
-              height: 300,
-              width: 300,
-              color: Colors.grey.shade300,
-              child: FlutterLogo(
-                size: 300,
-              ),
-            ),
-            BackdropFilter(
-              filter: ImageFilter.blur(
-                sigmaX: 3,
-                sigmaY: 3,
-              ),
-              child: Container(),
-            ),
-          ],
+        child: Container(
+          height: 300,
+          width: 300,
+          color: Colors.grey.shade300,
+          child: CustomPaint(
+            painter: MyPainter(),
+          ),
         ),
       ),
     );
+  }
+}
+
+class MyPainter extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    Paint myPaint = Paint()
+      ..color = Colors.red
+      ..strokeWidth = 3;
+
+    canvas.drawLine(
+      Offset(0, 0),
+      Offset(0, size.height),
+      myPaint,
+    );
+
+    canvas.drawLine(
+      Offset(0, size.height),
+      Offset(size.width, 0),
+      myPaint,
+    );
+
+    canvas.drawLine(
+      Offset(size.width, 0),
+      Offset(0, 0),
+      myPaint,
+    );
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
   }
 }
