4ef5267d0bdaeb2b23a01505ba2efc12284da2ed
xaiur <davinoverclock@gmail.com>
2022-12-06 14:33:16 +0800

    Carousel Slider

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,4 @@
-import 'dart:ui';
-
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -11,52 +10,64 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Custom Pain'),
+        title: Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Container(
-          height: 300,
-          width: 300,
-          color: Colors.grey.shade300,
-          child: CustomPaint(
-            painter: MyPainter(),
+      body: Column(
+        children: [
+          CarouselSlider(
+            options: CarouselOptions(
+              height: 300.0,
+              autoPlay: true,
+              autoPlayCurve: Curves.linearToEaseOut,
+              onPageChanged: (index, reason) => controller.index.value = index,
+            ),
+            items: [1, 2, 3, 4, 5].map(
+              (i) {
+                return Builder(
+                  builder: (BuildContext context) {
+                    return Container(
+                      width: MediaQuery.of(context).size.width,
+                      margin: EdgeInsets.symmetric(horizontal: 5.0),
+                      decoration: BoxDecoration(color: Colors.grey),
+                      child: Text(
+                        'text $i',
+                        style: TextStyle(fontSize: 16.0),
+                      ),
+                    );
+                  },
+                );
+              },
+            ).toList(),
           ),
-        ),
+          Obx(
+            () => Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [1, 2, 3, 4, 5].asMap().entries.map(
+                (entry) {
+                  return GestureDetector(
+                    onTap: () => controller.carouselC.animateToPage(entry.key),
+                    child: Container(
+                      width: 12.0,
+                      height: 12.0,
+                      margin:
+                          EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: (Theme.of(context).brightness ==
+                                      Brightness.dark
+                                  ? Colors.white
+                                  : Colors.black)
+                              .withOpacity(
+                                  controller.index == entry.key ? 0.9 : 0.4)),
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
+          ),
+        ],
       ),
     );
-  }
-}
-
-class MyPainter extends CustomPainter {
-  @override
-  void paint(Canvas canvas, Size size) {
-    Paint myPaint = Paint()
-      ..color = Colors.red
-      ..strokeWidth = 5
-      ..strokeCap = StrokeCap.round;
-
-    canvas.drawLine(
-      Offset(0, 0),
-      Offset(0, size.height),
-      myPaint,
-    );
-
-    canvas.drawLine(
-      Offset(0, size.height),
-      Offset(size.width, 0),
-      myPaint,
-    );
-
-    canvas.drawLine(
-      Offset(size.width, 0),
-      Offset(0, 0),
-      myPaint,
-    );
-  }
-
-  @override
-  bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    return false;
   }
 }
